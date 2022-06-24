18ff7c9b09a2e4dabeb7c418a6ff3791d0c50e82
andrewn77 <andrewnapiecekg@ip-172-31-20-167.ec2.internal>
2022-06-24 17:08:55 +0000

    Add security group

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "rhel" {
   ami           = "ami-096fda3c22c1c990a"
   instance_type = "t2.micro"
   key_name      = "${aws_key_pair.generated_key.key_name}"
+  vpc_security_group_ids = [aws_security_group.ab_sg.id]
   tags = {
     Name        = "terraform_instance"
   }
@@ -24,5 +25,29 @@ provisioner "local-exec" { # Create "myKey.pem" to your computer!!
     command = "echo '${tls_private_key.example.private_key_pem}' > ./myKey.pem"
   }
 }
+
+resource "aws_security_group" "ab_sg" {
+  name        = "allow_ssh"
+  vpc_id      = "vpc-0ce999ece68c5aa40"
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 
 
