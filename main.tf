787865db306f98222c3d22e993a5bfb658837a7d
andrewn77 <andrewnapiecekg@ip-172-31-20-167.ec2.internal>
2022-06-24 18:44:06 +0000

    Updating instance size in main.tf, and adding install.sh

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "rhel" {
+resource "aws_instance" "myubuntu" {
   ami           = "ami-052efd3df9dad4825"
   count         = 3
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name      = "${aws_key_pair.generated_key.key_name}"
   vpc_security_group_ids = [aws_security_group.ab_sg.id]
   tags = {
@@ -10,7 +10,7 @@ resource "aws_instance" "rhel" {
 }
 
 output "myEC2IP" { 
-  value = "${aws_instance.rhel.*.public_ip}"
+  value = "${aws_instance.myubuntu.*.public_ip}"
 }
 
 resource "tls_private_key" "example" {
